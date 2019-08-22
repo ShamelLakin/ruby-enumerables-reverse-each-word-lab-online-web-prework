f07e6f5d66901a3ba0b22bd4239d58cc45318b8d
Shamel Lakin <christlife4u@gmail.com>
2019-08-22 15:46:33 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
-def reverse_each_word[string]
-["Hello there, and how are you?"].reverse
+def reverse_each_word = ["Hello there, and how are you?"]
+  .reverse {|string| string}
 end
 
 def reverse_each_word[string]
